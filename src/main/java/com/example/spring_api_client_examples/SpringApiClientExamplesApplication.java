cba550763fad1a90e5e0f78d01831639507b19d9
DESKTOP-T69JMMQ\신헌우 <shinhunwoo1122@gmail.com>
2025-10-15 09:01:08 +0900

    최초 설정

---
+++ src/main/java/com/example/spring_api_client_examples/SpringApiClientExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_api_client_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiClientExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiClientExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_api_client_examples/SpringApiClientExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_api_client_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiClientExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
